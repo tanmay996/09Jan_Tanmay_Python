f08072ebca1cd2dffbb8bda6747265230a0456f2
tanmy996 <tanmaybhunia996@gmail.com>
2024-01-28 14:59:32 +0530

    assignments

--- Assignment/Module - 1/length of a string.py
+++ Assignment/Module - 1/length of a string.py
@@ -1,3 +1,3 @@
-# Write a Python program to calculate the length of a string
+## Write a Python program to calculate the length of a string
 strn = input("Enter a string for calculate length : ")
 print("Length of string is :",len(strn))
