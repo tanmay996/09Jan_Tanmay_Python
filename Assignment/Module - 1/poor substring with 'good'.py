a83363440244c325977bc4596c80c13355f15967
tanmy996 <tanmaybhunia996@gmail.com>
2024-02-07 22:56:29 +0530

    assignment

--- Assignment/Module - 1/poor substring with 'good'.py
+++ Assignment/Module - 1/poor substring with 'good'.py
@@ -2,7 +2,7 @@
 # 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the
 # whole 'not'...'poor' substring with 'good'. Return the resulting string
 
-str1 = "Hello! python is not so poor language"
+str1 = "Hello! not python is  so poor language"
 loc1 = str1.find("not")
 loc2 = str1.find("poor")
 print(str1.replace(str1[loc1:loc2+4],"good"))
