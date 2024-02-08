91b6d749af57d27760f97af33d8be292ec3dc157
tanmy996 <tanmaybhunia996@gmail.com>
2024-02-08 12:13:01 +0530

    assignment

--- Assignment/Module - 1/add 'ing' at the end of a given string.py
+++ Assignment/Module - 1/add 'ing' at the end of a given string.py
@@ -2,7 +2,7 @@
 should be at least 3). If the given string already ends with 'ing' then add
 'ly' instead if the string length of the given string is less than 3, leave it
 unchanged"""
-#
+##
 str1 = input("Enter a string : ")
 if len(str1) >= 3:
     if str1.endswith("ing") :
