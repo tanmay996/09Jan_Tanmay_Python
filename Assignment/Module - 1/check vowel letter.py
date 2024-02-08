8e392e0659754bd7004df518d4284cf19ea6df72
tanmy996 <tanmaybhunia996@gmail.com>
2024-02-08 08:05:47 +0530

    assignment

--- Assignment/Module - 1/check vowel letter.py
+++ Assignment/Module - 1/check vowel letter.py
@@ -1,4 +1,4 @@
-# Write a Python program to test whether a passed letter is a vowel or not.
+## Write a Python program to test whether a passed letter is a vowel or not.
 varLetter = input("Enter letter for check vowel or not : ")
 if varLetter == "a" or varLetter == "e" or varLetter == "i" or varLetter =="o" or varLetter == "u" or varLetter == "A" or varLetter == "E" or varLetter == "I" or varLetter =="" or varLetter == "U" :
     print(varLetter,"is Vowel")
